742358800a8b2b931b3a5abeb6f3258a7b6ec54c
CaioQC <129068301+CaioQC@users.noreply.github.com>
2024-06-12 17:45:51 -0300

    Update main.c

--- atividade_cidades/main.c
+++ atividade_cidades/main.c
@@ -9,26 +9,10 @@ int main() {
     
     Estrada *estrada = getEstrada(arquivo);
     
-    if (estrada == NULL) {
-        printf("Erro ao ler arquivo\n");
-        return 1;
-    }
+    calcularMenorVizinhanca(arquivo);
     
-    double menorVizinhanca = calcularMenorVizinhanca(arquivo);
-    char *cidade = cidadeMenorVizinhanca(arquivo);
+    cidadeMenorVizinhanca(arquivo);
+
+	return 0;
     
-    printf("Comprimento da estrada: %d\n", estrada->T);
-    printf("Número de cidades: %d\n", estrada->N);
-    
-    for(int i = 0; i < estrada->N; i++){
-        printf("Posição: %d, Cidade: %s\n", estrada->C[i].Posicao, estrada->C[i].Nome);
-    }
-    
-    printf("Menor vizinhança: %.2f\n", menorVizinhanca);
-    printf("Cidade com menor vizinhança: %s\n", cidade);
-    
-    free(estrada->C);
-    free(estrada);
-    
-    return 0;
 }
